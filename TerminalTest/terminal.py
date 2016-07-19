fcc4c8ebdcfe657d72231ca01f4c805069ed3196
DiscoreMe <nikitabikmaev@gmail.com>
2016-07-19 12:06:46 +0300

    Add file terminal.py

---
+++ TerminalTest/terminal.py
@@ -0,0 +1,14 @@
+#Main File
+from TerminalSimulator.TerminalTest.Terminal.core.terminal import Terminal
+data = None
+while True:
+    user = input('Please, enter login: ')
+    if user == '':
+        data = {'user': None, 'pass': None}
+        break
+    password = input('Please, enter password: ')
+    data = {'user': user, 'pass': password}
+    break
+
+terminal = Terminal(data=data, r_t='r-1')
+terminal.getWarnings()
