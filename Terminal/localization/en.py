a91ec2bda849ea1c124dc02fb601f83334fbc6ca
DiscoreMe <nikitabikmaev@gmail.com>
2016-07-24 13:08:18 +0300

    Adding localization English

---
+++ Terminal/localization/en.py
@@ -0,0 +1,35 @@
+start_terminal = 'Launch Terminal...'
+login_in_system = 'Login in system'
+auth_user = 'User Authorization ...'
+user_login_in_guest = 'A user logged as a \"Guest\"'
+user_login = '{} came as a \"{}\"'
+wrong_password = 'Wrong password'
+account_not_exists = 'This account does not exist. A user logged as a \"Guest\"'
+press_enter = 'Press Enter...'
+disk_name_future = 'Enter the future name of the drive'
+name_reserved = 'This name is reserved system'
+creating_disk = 'Creating a disk'
+disk_create = 'Disk {} set'
+disk_not_exists_next_menucreate = 'Drive exists. You switch to create a disc menu'
+available_disks = 'Available drives'
+disks = 'Disks'
+enter_name_disk_on_connect = 'Enter the disc name to connect'
+connecting_disk = 'Connection to the drive{}'
+connect_successfully = 'You have successfully connected!'
+disk_name_not_exists = 'Drive {} does not exist'
+list_empty = 'The list is empty'
+list_folders = '[Folder List]'
+list_files = '[List of files]'
+folder_creation = 'mkdir {name} {...} - folder creation'
+dir_not_exists = 'This directory does not exist'
+word_rez_system = 'The word is {} reserved system'
+dir_exists = 'Folder {} already exists'
+dir_created = 'Folder {} successfully created'
+command_not_exist = 'This command does not exist'
+print_help = [
+    'q - exit terminal',
+    'help - assistance teams. There are two ways: help or help {command}',
+    'cd {путь} - moving directories disc',
+    'ls - it displays the available folders and files in the current directory',
+    'mkdir {name} {...} - folder creation. mkdir 1 2 - at the same time creates a folder \"1\" and \"2\"'
+]
