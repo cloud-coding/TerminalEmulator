3006839cb7113a5d685bfb3f8942abf7e954ba49
DiscoreMe <nikitabikmaev@gmail.com>
2016-08-26 12:12:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from Terminal.core.terminal import Terminal
 terminal = Terminal()
 terminal.setLocale('ru')
 terminal.setVersion('1.2')
+terminal.setUser('nikita', '123456')
+terminal.run_disk()
+terminal.run()
