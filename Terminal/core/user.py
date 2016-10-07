e43de1b5ad815326ce37bae8000e4713a1657dca
DiscoreMe <nikitabikmaev@gmail.com>
2016-10-07 21:36:03 +0300

    Add user.py

---
+++ Terminal/core/user.py
@@ -0,0 +1,14 @@
+class User:
+    def __init__(self, user, password):
+        self.user = user
+        self.password = password
+
+
+    def loadUser(self):
+        import sqlite3
+
+        pass
+
+
+    def saveUser(self):
+        pass
