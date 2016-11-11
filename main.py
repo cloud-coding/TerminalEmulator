f0b9aac31d59d55b0fcd2cdefec57271c222e6e0
DiscoreMe <nikitabikmaev@gmail.com>
2016-11-11 19:42:39 +0300

    Удалена функция установки версии.\

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from Terminal.core.terminal import Terminal
 
 terminal = Terminal()
 terminal.setLocale('ru')
-terminal.setVersion('0.9.40-dev')
 terminal.setUser('nikita', '123456')
 terminal.run_disk()
 terminal.run()
